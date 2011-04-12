3418ee5d6744d44496ea21b90e6eef4999676a38
Kevin Nuut (L) <git@kevinnuut.com>
2011-04-12 18:44:22 -0500

    generator code

--- public/js/generator.js
+++ public/js/generator.js
@@ -10,6 +10,8 @@ $(function() {
 			'htile': 13
 		}
 	}
+	
+	var map = {};
 			
 	$('#game').canvas();
 	
@@ -17,25 +19,62 @@ $(function() {
 	var h  = $('#game').canvasHeight();
 	var xc = w / 2;
 	var yc = h / 2;	
-		
+	
+	var zstep  = 12,
+		zlimit = zstep * 8,
+		ystep  = 0,
+		xstep  = 0, 
+		steps  = 12;
+	
 	terrain.dirt.obj        = new Image();
 	terrain.dirt.obj.onload = render;	
 	terrain.dirt.obj.src    = imgDir + terrain.dirt.url;
 	
 	function render() {
 		var context = $('#game').canvasContext();
-		
-		var ystep, xstep = 0, steps = 24;
-		for (var ystep = -steps; ystep <= steps; ystep++) {
-			for (var xstep = -steps; xstep <= steps; xstep++) {
-				var zoff = Math.round(Math.random() * 4) * 4;
+				
+		for (var xstep = -steps; xstep <= steps; xstep++) {
+			map[xstep] = {};
+			
+			for (var ystep = -steps; ystep <= steps; ystep++) {			
+				map[xstep][ystep] = {};
+				
+				var zrange = searchNeighbors(xstep, ystep);
+				var z      = 0;
+				
+				if (!zrange) {
+					z = Math.round(Math.random() * zstep - (zstep / 2)) * zstep;
+				} else {
+					var zoff  = (zrange.zmax - zrange.zmin);
+					var zrand = Math.random() * zoff + zrange.zmin;
+					console.log(zrange.zmax + ' ' + zrange.zmin + ' ' + zrand);
+					z = Math.round((Math.random() * zoff + zrange.zmin) / zstep) * zstep;
+				}
+				
 				var xpos = xc + xstep * terrain.dirt.xoff - ystep * terrain.dirt.xoff;
-				var ypos = yc + xstep * terrain.dirt.yoff + ystep * terrain.dirt.yoff + zoff;
+				var ypos = yc + xstep * terrain.dirt.yoff + ystep * terrain.dirt.yoff - z;
+				
+				map[xstep][ystep].z = z;
 				
 				context.drawImage(terrain.dirt.obj, xpos, ypos);
 			}
 		}
-		
-		
 	};
+	
+	function searchNeighbors(xstep, ystep)
+	{
+		var zmax = -zlimit, zmin = zlimit;
+		
+		for (var xtemp = -1; xtemp <= 1; xtemp++) {
+			for (var ytemp = -1; ytemp <= 1; ytemp++) {
+				var x = xstep + xtemp, y = ystep + ytemp;
+				if ((xtemp != 0 || ytemp != 0) && x in map && y in map[x]) {
+					zmax = Math.max(zmax, map[x][y].z);
+					zmin = Math.min(zmin, map[x][y].z);
+				}
+			}
+		}
+		
+		return (zmax != -zlimit ? {'zmax': zmax - zstep, 'zmin': zmin + zstep} : null);
+	}
 });
